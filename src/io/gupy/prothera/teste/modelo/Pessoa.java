e51f72888a1077f76b49e5ec6e0fbbeca59d6ba8
michel diniz <micheldiniz@ibc.gov.br>
2024-06-14 15:07:22 -0300

    Implementação da classe Pessoa

--- src/io/gupy/prothera/teste/modelo/Pessoa.java
+++ src/io/gupy/prothera/teste/modelo/Pessoa.java
@@ -1,5 +1,32 @@
 package io.gupy.prothera.teste.modelo;
 
+import java.time.LocalDate;
+
 public class Pessoa {
 
+	private String nome;
+	private LocalDate dataNascimento;
+	
+	public Pessoa(String nome, LocalDate dataNascimento) {
+		super();
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+			
 }
