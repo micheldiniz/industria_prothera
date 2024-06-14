3b0f91c1a9592bc1132c08d895eaf321f1577649
michel diniz <micheldiniz@ibc.gov.br>
2024-06-14 15:07:10 -0300

    Implementação da classe Funcionario

--- src/io/gupy/prothera/teste/modelo/Funcionario.java
+++ src/io/gupy/prothera/teste/modelo/Funcionario.java
@@ -1,5 +1,15 @@
 package io.gupy.prothera.teste.modelo;
 
-public class Funcionario extends Pessoa{
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+public class Funcionario extends Pessoa{
+	
+	public Funcionario(String nome, LocalDate dataNascimento) {
+		super(nome, dataNascimento);
+		// TODO Auto-generated constructor stub
+	}
+
+	private BigDecimal salario;
+	private String funcao;
 }
