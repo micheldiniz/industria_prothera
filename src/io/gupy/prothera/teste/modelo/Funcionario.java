dcd23655ed2464548ce95e098c94f23c3333788e
michel diniz <micheldiniz@ibc.gov.br>
2024-06-14 15:53:09 -0300

    metodo equals e hash da classe Funcionario

--- src/io/gupy/prothera/teste/modelo/Funcionario.java
+++ src/io/gupy/prothera/teste/modelo/Funcionario.java
@@ -2,14 +2,44 @@ package io.gupy.prothera.teste.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Funcionario extends Pessoa{
 	
-	public Funcionario(String nome, LocalDate dataNascimento) {
-		super(nome, dataNascimento);
-		// TODO Auto-generated constructor stub
-	}
-
 	private BigDecimal salario;
 	private String funcao;
+	
+	public Funcionario(String nome, LocalDate dataNascimento) {
+		super(nome, dataNascimento);
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(funcao, salario);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Funcionario other = (Funcionario) obj;
+		return Objects.equals(funcao, other.funcao) && Objects.equals(salario, other.salario);
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
+	
 }
